fcb738e162d66a26926b181bfa12272b4a86c023
Thomas Martin <thomas@classkick.com>
2023-07-07 14:07:45 -0500

    fixes map

--- js/scripts.js
+++ js/scripts.js
@@ -241,7 +241,8 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+
+    var location = {lat: 43.027909184816714, lng: -89.34318410683845};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
